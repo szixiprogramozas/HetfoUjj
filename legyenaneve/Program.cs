1862445eb2f9bd228acc1ffd9192094d6d1ca3fe
Olivér Imre Ujj <ujjolive@stud.szixi.hu>
2021-02-08 12:07:39 +0100

    "első sablon"

---
+++ legyenaneve/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace legyenaneve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine("3. feladat:");
+            Console.WriteLine("4. feladat:");
+            Console.WriteLine("5. feladat:");
+            Console.WriteLine("6. feladat:");
+            Console.WriteLine("7. feladat:");
+            Console.WriteLine("8. feladat:");
+        }
+    }
+
+    static void masodikfel()
+    {
+    
+    
+    }
+
+    static void harmadikfel()
+    {
+    
+    
+    }
+
+    static void negyedikfel()
+    {
+    
+    
+    }
+
+    static void otodikfel()
+    {
+    
+    
+    }
+
+    static void hatodikfel()
+    {
+    
+    
+    }
+
+    static void hetedikfel()
+    {
+    
+    
+    }
+
+    static void nyolcadikfel()
+    {
+    
+    
+    }
+}
